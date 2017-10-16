e22a7eff0ab11978f91298a92a7a89cf31b6506e
Matteo Zancanaro <matteo.zancanaro@mail.polimi.it>
2017-10-16 15:37:48 +0200

    prova

---
+++ exercises/02_arrays/reverse.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <math.h> 
+
+int main() {
+  std::cout << "Please tap the dimension of the array you want to be reversed\n";
+  int dim;
+  std::cin >> dim;
+  double array[dim];
+  std::cin.clear();
+  std::cout << "Please, insert the desired array\n";
+  std::cin >> array;
+  }
+  
+  while(floor(integer)!=integer)
+  {
+  std::cout << "Error: the number is not an integer\n";
+  std::cout << "Please, insert an integer\n";
+  std::cin.clear();
+  std::cin.ignore();
+  std::cin >> integer;
+  }
+  
+  std::cout << "Fine," << " [" << integer << "] is a good choice\n";
+  
+  return 0;
+}
